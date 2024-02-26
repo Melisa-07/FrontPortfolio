fe42837bb528d6e59b3ed22e0bfaf51323f4a043
Melisa Alfonso <alfonsomelisa@hotmail.com>
2024-02-26 20:02:07 -0300

    inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { ExperiencialaboralComponent } from './experiencialaboral/experiencialab
 import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
-  { path: '', component: InicioComponent },
+  { path: 'inicio', component: InicioComponent },
   { path: 'login', component: LoginComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: 'estudios', component: EstudiosComponent },
